ff561d2bab4ec8cbae6fd756190c3036a12f6fdf
Cazenove <707533554@qq.com>
2020-03-11 19:53:31 +0800

    0.2
    分离json

--- backend/spider/spider.py
+++ backend/spider/spider.py
@@ -6,5 +6,20 @@ def get_city_date(word=None):
     response = requests.post(url,data=form_data)
     #将Json格式字符串转字典
     content = json.loads(response.text)
+    result = content['result']
     print(content)
+    move_in_list = result['moveInList']
+    move_out_list = result['moveOutList']
+    print("迁入城市")
+    for i in move_in_list:
+        city_name = i['city_name']
+        province_name = i['province_name']
+        city_code = i['city_code']
+        print(city_name+" "+province_name+" "+city_code)
+    print("迁出城市")
+    for i in move_out_list:
+        city_name = i['city_name']
+        province_name = i['province_name']
+        city_code = i['city_code']
+        print(city_name+" "+province_name+" "+city_code)
 get_city_date()
